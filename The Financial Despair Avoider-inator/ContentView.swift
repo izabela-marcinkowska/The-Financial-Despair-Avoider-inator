7df012a6e55c19cf87613d836d69f43c76fdadc4
Devy <izabela.marcinkowska@outlook.com>
2024-12-06 13:02:07 +0100

    first commit

---
+++ The Financial Despair Avoider-inator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Financial Despair Avoider-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The Financial Despair Avoider-inator/The_Financial_Despair_Avoider_inatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Financial_Despair_Avoider_inatorApp.swift
+//  The Financial Despair Avoider-inator
+//
+//  Created by Izabela Marcinkowska on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct The_Financial_Despair_Avoider_inatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
