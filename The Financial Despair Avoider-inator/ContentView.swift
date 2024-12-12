b528dd5569dbad058f2dec480ff2d8efbf42efb0
Devy <izabela.marcinkowska@outlook.com>
2024-12-12 09:02:36 +0100

    feat: Show list of expenses on the view

--- The Financial Despair Avoider-inator/ContentView.swift
+++ The Financial Despair Avoider-inator/ContentView.swift
@@ -8,13 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    var expenses: [Expense] = []
+    var expenses = Expenses().expenses
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(expenses) {
+                Text($0.name)
+            }
         }
         .padding()
     }
